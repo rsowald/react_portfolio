a561dc74d767bfa6f4d44ee5b0bc1ab185d8ecfa
rsowald <72467920+rsowald@users.noreply.github.com>
2021-05-11 00:00:48 -0700

    start navbar -not functional yet

---
+++ client/src/components/Nav/index.js
@@ -0,0 +1,25 @@
+import Nav from 'react-bootstrap/Nav';
+
+function Navbar() {
+    return (
+        <Nav className="justify-content-end" activeKey="/home">
+            <Nav.Item>
+                <Nav.Link href="/home">Home</Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+                <Nav.Link eventKey="link-1">Portfolio</Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+                <Nav.Link eventKey="link-2">Resume</Nav.Link>
+            </Nav.Item>
+            <NavDropdown title="Social Media" id="nav-dropdown">
+                <NavDropdown.Item eventKey="4.1">GitHub</NavDropdown.Item>
+                <NavDropdown.Item eventKey="4.2">LinkedIn</NavDropdown.Item>
+                <NavDropdown.Divider />
+                <NavDropdown.Item eventKey="4.3">Instagram</NavDropdown.Item>
+            </NavDropdown>
+        </Nav>
+    )
+}
+
+export default Navbar;
