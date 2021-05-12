cae05802d55a31205696b3bffe33adbd09f0bad1
rsowald <72467920+rsowald@users.noreply.github.com>
2021-05-11 23:11:59 -0700

    finish nav bar content

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -1,24 +1,27 @@
 import React from 'react';
 import Nav from 'react-bootstrap/Nav';
 import NavDropdown from 'react-bootstrap/NavDropdown';
+import "./style.css";
 
 function Navbar() {
     return (
-        <Nav className="justify-content-end" activeKey="/home">
+        <Nav variant="tabs" className="justify-content-end color" defaultActiveKey="/home">
             <Nav.Item>
-                <Nav.Link href="/home">Home</Nav.Link>
+                <Nav.Link eventKey="/home" style={{ color: 'white' }}>Home</Nav.Link>
             </Nav.Item>
             <Nav.Item>
-                <Nav.Link eventKey="link-1">Portfolio</Nav.Link>
+                <Nav.Link eventKey="/portfolio" style={{ color: 'white' }}>Portfolio</Nav.Link>
             </Nav.Item>
             <Nav.Item>
-                <Nav.Link eventKey="link-2">Resume</Nav.Link>
+                <Nav.Link eventKey="/Resume" style={{ color: 'white' }}>Resume</Nav.Link>
             </Nav.Item>
-            <NavDropdown title="Social Media" id="nav-dropdown">
-                <NavDropdown.Item eventKey="4.1">GitHub</NavDropdown.Item>
-                <NavDropdown.Item eventKey="4.2">LinkedIn</NavDropdown.Item>
+            <NavDropdown title={
+                <span className="text-white my-auto">Social Media</span>
+            } id="nav-dropdown">
+                <NavDropdown.Item href="https://github.com/rsowald">GitHub</NavDropdown.Item>
+                <NavDropdown.Item href="https://www.linkedin.com/in/renae-sowald">LinkedIn</NavDropdown.Item>
                 <NavDropdown.Divider />
-                <NavDropdown.Item eventKey="4.3">Instagram</NavDropdown.Item>
+                <NavDropdown.Item href="https://www.instagram.com/renaesowald/">Instagram</NavDropdown.Item>
             </NavDropdown>
         </Nav>
     )
