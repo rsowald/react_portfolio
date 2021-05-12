641378096915312c990fa95ae4e81827f18dc9e7
rsowald <72467920+rsowald@users.noreply.github.com>
2021-05-12 00:59:12 -0700

    switching nav to navbar

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
 /* eslint-disable react/prefer-stateless-function */
 import React, { Component } from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import Nav from "./components/Nav";
+import MyNav from "./components/Nav";
 import NoMatch from "./pages/NoMatch";
 import Home from "./pages/Home";
 import About from "./pages/About";
@@ -13,7 +13,7 @@ class App extends Component {
   render() {
     return (
       <Router>
-        <Nav />
+        <MyNav />
         <Switch>
           <Route exact path="/" component={Home} />
           <Route exact path="/about" component={About} />
--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -1,30 +1,31 @@
 import React from 'react';
-import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav'
 import NavDropdown from 'react-bootstrap/NavDropdown';
 import "./style.css";
 
-function Navbar() {
+function MyNav() {
     return (
-        <Nav variant="tabs" className="justify-content-end color" defaultActiveKey="/home">
-            <Nav.Item>
-                <Nav.Link eventKey="/home" style={{ color: 'white' }}>Home</Nav.Link>
-            </Nav.Item>
-            <Nav.Item>
-                <Nav.Link eventKey="/portfolio" style={{ color: 'white' }}>Portfolio</Nav.Link>
-            </Nav.Item>
-            <Nav.Item>
-                <Nav.Link eventKey="/Resume" style={{ color: 'white' }}>Resume</Nav.Link>
-            </Nav.Item>
-            <NavDropdown title={
-                <span className="text-white my-auto">Social Media</span>
-            } id="nav-dropdown">
-                <NavDropdown.Item href="https://github.com/rsowald">GitHub</NavDropdown.Item>
-                <NavDropdown.Item href="https://www.linkedin.com/in/renae-sowald">LinkedIn</NavDropdown.Item>
-                <NavDropdown.Divider />
-                <NavDropdown.Item href="https://www.instagram.com/renaesowald/">Instagram</NavDropdown.Item>
-            </NavDropdown>
-        </Nav>
+
+        <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark" className="space">
+            <Navbar.Brand href="#home">Renae Sowald</Navbar.Brand>
+            <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+            <Navbar.Collapse id="responsive-navbar-nav">
+                <Nav className="ml-auto">
+                    <Nav.Link href="/#portfolio">Portfolio</Nav.Link>
+                    <Nav.Link href="/#about">About</Nav.Link>
+                    <Nav.Link href="/#resume">Resume</Nav.Link>
+                    <NavDropdown title="Social Media" id="collapsible-nav-dropdown">
+                        <NavDropdown.Item href="https://github.com/rsowald" target={"_blank"} rel={"noopener noreferrer"}>GitHub</NavDropdown.Item>
+                        <NavDropdown.Item href="https://www.linkedin.com/in/renae-sowald" target={"_blank"} rel={"noopener noreferrer"}>LinkedIn</NavDropdown.Item>
+                        <NavDropdown.Divider />
+                        <NavDropdown.Item href="https://www.instagram.com/renaesowald/" target={"_blank"} rel={"noopener noreferrer"}>Instagram</NavDropdown.Item>
+                    </NavDropdown>
+                </Nav>
+
+            </Navbar.Collapse>
+        </Navbar>
     )
 }
 
-export default Navbar;
+export default MyNav;
