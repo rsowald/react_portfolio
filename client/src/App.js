c991cd11af472acbc58f86cadfc4e0caa5dc0501
rsowald <72467920+rsowald@users.noreply.github.com>
2021-05-11 23:20:55 -0700

    created routes for "pages"

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,6 @@
 /* eslint-disable react/prefer-stateless-function */
 import React, { Component } from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Nav from "./components/Nav";
 import "./App.css";
 import { Container } from "react-bootstrap";
@@ -8,10 +9,18 @@ import { Container } from "react-bootstrap";
 class App extends Component {
   render() {
     return (
-      <Container fluid style={{ backgroundColor: "gray" }}>
-        <Nav />
-      </Container>
-
+      <Router>
+        <Container fluid style={{ backgroundColor: "gray" }}>
+          <Nav />
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route exact path="/about" component={About} />
+            <Route exact path="/portfolio" component={Portfolio} />
+            <Route exact path="/resume" component={Resume} />
+            <Route component={NoMatch} />
+          </Switch>
+        </Container>
+      </Router>
     );
   }
 }
