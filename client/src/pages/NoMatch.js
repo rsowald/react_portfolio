9e40448330ccf338212cd4e95fce6d2daf94c12d
rsowald <72467920+rsowald@users.noreply.github.com>
2021-05-12 00:59:54 -0700

    fix home link

--- client/src/pages/NoMatch.js
+++ client/src/pages/NoMatch.js
@@ -3,12 +3,12 @@ import { Container, Row, Col, Jumbotron } from "react-bootstrap";
 
 function NoMatch() {
     return (
-        <Container fluid style={{ backgroundColor: "gray" }}>
+        <Container fluid >
             <Row>
                 <Col size="md-12">
                     <Jumbotron>
                         <h1>404 Page Not Found</h1>
-                        <a src="/home">Try going back to the home page</a>
+                        <a href="/">Try going back to the home page</a>
                     </Jumbotron>
                 </Col>
             </Row>
