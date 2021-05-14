b7e7c44ce46aa2803f1112ae9de5ea20b25e45c3
rsowald <72467920+rsowald@users.noreply.github.com>
2021-05-14 01:02:33 -0700

    webpack

---
+++ client/build/precache-manifest.e09c535ddb58202d1a3658751a8a26b6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "6293cf87b482d5d9d037",
+    "url": "/react_portfolio/static/css/main.04fa1dd6.chunk.css"
+  },
+  {
+    "revision": "6293cf87b482d5d9d037",
+    "url": "/react_portfolio/static/js/main.334a2b80.chunk.js"
+  },
+  {
+    "revision": "1416a318fd302ab9db4f",
+    "url": "/react_portfolio/static/js/runtime~main.d4bcd7b8.js"
+  },
+  {
+    "revision": "62dda8b09a30657aaf0c",
+    "url": "/react_portfolio/static/css/2.dd1c4a24.chunk.css"
+  },
+  {
+    "revision": "62dda8b09a30657aaf0c",
+    "url": "/react_portfolio/static/js/2.cde8ae51.chunk.js"
+  },
+  {
+    "revision": "3d28f3cec7a816396c67f790be9f0399",
+    "url": "/react_portfolio/index.html"
+  }
+];
