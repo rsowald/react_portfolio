755f8e14239c4348172515a32c53812002c0e6ec
rsowald <72467920+rsowald@users.noreply.github.com>
2021-05-13 23:33:24 -0700

    card component using props

---
+++ client/src/components/ProjectCard/index.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Card } from "react-bootstrap";
+
+function ProjectCard(props) {
+    return (
+
+        <Card>
+            <Card.Img variant="top" src={props.image} alt="Screenshot of the project." />
+            <Card.Body>
+                <Card.Title>{props.title}</Card.Title>
+                <Card.Text>
+                    <a href={props.demo} target={"_blank"}>Demo</a> • <a href={props.repo} target={"_blank"}>GitHub Repo</a>
+                </Card.Text>
+            </Card.Body>
+            <Card.Footer>
+                <small className="text-muted">{props.text}</small>
+            </Card.Footer>
+        </Card>
+
+    )
+}
+
+export default ProjectCard;
