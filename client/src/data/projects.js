4e312bd998a04033c58a4a79c93bb23ce5bbbcc0
rsowald <72467920+rsowald@users.noreply.github.com>
2021-05-14 00:22:47 -0700

    adding project data

---
+++ client/src/data/projects.js
@@ -0,0 +1,65 @@
+export default [
+    {
+        title: "GrammaGram",
+        image: "/portfolioScreenshots/grammagram.jpg",
+        demo: "https://boiling-savannah-28129.herokuapp.com/",
+        repo: "https://github.com/rsowald/GrammaGram",
+        text: "Full stack with MySQL database, Handlebars.js views, Passport.js authentication, and an Express server deployed to Heroku"
+    },
+    {
+        title: "Meal Planner",
+        image: "portfolioScreenshots/Meal_Planner.jpg",
+        demo: "https://rsowald.github.io/MealPlanner/",
+        repo: "https://github.com/rsowald/MealPlanner",
+        text: "AJAX calls to third party APIs, jQuery, JavaScript modal, CSS"
+    },
+    {
+        title: "I Blog Technically",
+        image: "/portfolioScreenshots/tech_blog.jpg",
+        demo: "https://rhubarb-cobbler-10166.herokuapp.com/",
+        repo: "https://github.com/rsowald/TechBlog",
+        text: "Full stack with MySQL database, Handlebars.js views, and an Express server deployed to Heroku"
+    },
+    {
+        title: "Weather Dashboard",
+        image: "/portfolioScreenshots/Weather_Dashboard.jpg",
+        demo: "https://rsowald.github.io/WeatherDashboard/",
+        repo: "https://github.com/rsowald/WeatherDashboard",
+        text: "AJAX calls to third party APIs, jQuery and CSS"
+    },
+    {
+        title: "React Employee Directory",
+        image: "/portfolioScreenshots/employee_directory.jpg",
+        demo: "https://rsowald.github.io/employee_directory/",
+        repo: "https://github.com/rsowald/employee_directory",
+        text: "React app, sortable tables, onChange filter, react-bootstrap"
+    },
+    {
+        title: "Workout Tracker",
+        image: "/portfolioScreenshots/workout_tracker.jpg",
+        demo: "https://gentle-coast-08368.herokuapp.com/",
+        repo: "https://github.com/rsowald/WorkoutTracker",
+        text: "Mongo Db, Chart.js, Express server"
+    },
+    {
+        title: "Workday Planner",
+        image: "portfolioScreenshots/Workday_Scheduler.jpg",
+        demo: "https://rsowald.github.io/WorkDayPlanner/",
+        repo: "https://github.com/rsowald/WorkDayPlanner",
+        text: "JQuery, jQuery DOM manipulation, and local storage"
+    },
+    {
+        title: "Team Profile Generator",
+        image: "portfolioScreenshots/team_profile_generator.jpg",
+        demo: "assets/portfolioScreenshots/team_profile_generator.jpg",
+        repo: "https://github.com/rsowald/TeamProfileGenerator",
+        text: "Command Line App to generate styled HTML"
+    },
+    {
+        title: "Employee Tracker CMS",
+        image: "portfolioScreenshots/employee_tracker.jpg",
+        demo: "https://github.com/rsowald/EmployeeTrackerCMS",
+        repo: "https://github.com/rsowald/EmployeeTrackerCMS",
+        text: "Command Line App to track employees, their roles, and their managers"
+    }
+]
