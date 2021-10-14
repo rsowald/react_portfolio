79f7e9470b52728a08ad167b5b5c0bfc99ab1251
rsowald <72467920+rsowald@users.noreply.github.com>
2021-10-14 10:04:19 -0700

    Updates

--- precache-manifest.a32a5c95f4a23ebd62cd427fadd7a187.js
+++ precache-manifest.a32a5c95f4a23ebd62cd427fadd7a187.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = [
   {
-    "revision": "15f4bd65d6c3655b9038",
+    "revision": "24d44369e6a2b3e138fe",
     "url": "/react_portfolio/static/css/main.c5908d73.chunk.css"
   },
   {
-    "revision": "15f4bd65d6c3655b9038",
-    "url": "/react_portfolio/static/js/main.2448150b.chunk.js"
+    "revision": "24d44369e6a2b3e138fe",
+    "url": "/react_portfolio/static/js/main.587ad2ac.chunk.js"
   },
   {
     "revision": "1416a318fd302ab9db4f",
@@ -20,7 +20,7 @@ self.__precacheManifest = [
     "url": "/react_portfolio/static/js/2.cde8ae51.chunk.js"
   },
   {
-    "revision": "02d348251b8a0baa92ae092b0367373a",
+    "revision": "6106bc64d05c0b39e193b51a21b1da30",
     "url": "/react_portfolio/index.html"
   }
 ];
