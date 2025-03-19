6a2b32b659fb26f6f246cb4bc058f5eaa91414ee
Renae Sowald <renaesowald@gmail.com>
2025-03-18 17:36:08 -0700

    remove outdated bootstrap

--- client/src/pages/NoMatch.js
+++ client/src/pages/NoMatch.js
@@ -1,19 +1,19 @@
 import React from "react";
-import { Container, Row, Col, Jumbotron } from "react-bootstrap";
+import { Container, Row, Col } from "react-bootstrap";
 
 function NoMatch() {
-    return (
-        <Container fluid style={{ paddingBottom: "600px" }}>
-            <Row>
-                <Col size="md-12">
-                    <Jumbotron>
-                        <h1>404 Page Not Found</h1>
-                        <a href="/">Try going back to the home page</a>
-                    </Jumbotron>
-                </Col>
-            </Row>
-        </Container>
-    )
+  return (
+    <Container fluid style={{ paddingBottom: "600px" }}>
+      <Row>
+        <Col size="md-12">
+          <div className="container-fluid py-5">
+            <h1>404 Page Not Found</h1>
+            <a href="/">Try going back to the home page</a>
+          </div>
+        </Col>
+      </Row>
+    </Container>
+  );
 }
 
 export default NoMatch;
